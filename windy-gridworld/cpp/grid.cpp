95f7b1b58ba6d3be7a99c285dd8e3b2b6ae1be5d
lhugens <leonardolobato@protonmail.com>
2020-07-29 17:17:59 +0100

    added policy and started main run()

--- windy-gridworld/cpp/grid.cpp
+++ windy-gridworld/cpp/grid.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <algorithm>
  
 #define K 	100
 #define alpha 	0.4
@@ -12,14 +13,14 @@ uniform_real_distribution<double> uniform_dist_double{0., 1.};
 uniform_int_distribution<> uniform_dist_int{0, 3};
 
 struct walker{
-    int pos[2] = {0, 3};
-    int newpos[2] = {0, 3};
-    unsigned act = 2;
-    unsigned newact = 2;
+    int pos[2];
+    int newpos[2];
+    unsigned act;
+    unsigned newact;
 
     float moves[4][2] = {{0, -1}, {1, 0}, {-1, 0}, {0, 1}};
-    float elig[7][10][4] = {0};
-    float value[7][10][4] = {0};
+    float elig[7][10][4];
+    float value[7][10][4];
     float epsi;
 
     void is_inside(){
@@ -67,15 +68,54 @@ struct walker{
     }
 
     void choose_action(){
-        newact = get_rint();
+        if(get_rdouble() > epsi){
+            int i = newpos[0], j = newpos[1];
+            newact = distance(value[i][j], max_element(value[i][j], value[i][j] + 4));
+            cout << "greedy" << endl;
+        } else {
+            newact = get_rint();
+            cout << "random" << endl;
+        }
     }
 
+    void run(){
+
+        // initialize Q(s, a) arbitrarily
+        value = {0}; 
+
+        for(int k = 0; k != 100; k++){ // for each episode
+
+            // initialize eligilibily = 0 for all s,a
+            elig = {0};
+
+            // initialize state S
+            pos = {0, 3}; 
+
+            // initialize action A
+            int i = pos[0], j = pos[1];
+            act = distance(value[i][j], max_element(value[i][j], value[i][j] + 4)); 
+            
+            //while !terminated(){
+
+            //}
+        }
+    }
+
+
+
+    //int pos[2] = {0, 3};
+    //int newpos[2] = {0, 3};
+    //unsigned act = 2;
+    //unsigned newact = 2;
+
+    //float moves[4][2] = {{0, -1}, {1, 0}, {-1, 0}, {0, 1}};
+    //float elig[7][10][4] = {0};
+    //float value[7][10][4] = {0};
+    //float epsi = 0.5;
 
 };
 
 int main(){
     walker bot;
-
-    cout << bot.newact << endl;
-    bot.choose_action();
+    bot.run();
 }
